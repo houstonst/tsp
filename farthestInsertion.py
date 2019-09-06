f6137c57889e126c2444f37696b60f3bbcb4108f
houstonst <matthew.houston@westpoint.edu>
2019-09-06 00:25:11 -0400

    Inserts the farthest node in the cheapest way possible

--- farthestInsertion.py
+++ farthestInsertion.py
@@ -29,7 +29,35 @@ def farthestNeighbor(graph, nameArray):
         if c in path and wg[i][c] < minLeg: #consider only edges that go from n to a subtour node that are less than the current minLeg
           minLeg = wg[i][c]
       legList.append((minLeg, i))
-  return legList
+  maxMinLeg = -1
+  nextNode = -1
+  for pair in legList:
+    if pair[0] > maxMinLeg:
+      maxMinLeg = pair[0]
+      nextNode = pair[1]
+  
+  #see which edge can be broken and connected to this farthest node in the cheapest manner
+  edgeList = []
+  for r in range(len(wg)//2):
+    for c in range(len(wg)):
+      if r in path and c in path and r != c: #consider edges that exist in the subtour and are not self-directed
+        edgeList.append((wg[r][c], r, c)) #create a list of edges and the incident nodes that exist in the subtour
+  minDamage = 999999
+  deletedEdge = (-1, -1)
+  addedEdge1 = -1
+  addedEdge2 = -1
+  for edge in edgeList:
+    pEdge1 = wg[nextNode][edge[1]]
+    pEdge2 = wg[nextNode][edge[2]]
+    damage = cost - edge[0] + pEdge1 + pEdge2
+    if damage < minDamage:
+      minDamage = damage
+      deletedEdge = (edge[1], edge[2])
+      addedEdge1 = (nextNode, edge[1])
+      addedEdge2 = (nextNode, edge[2])
+  print("The edge {} can be deleted and edges {}, {} can be added for a new cost of {} in place of the former cost {}".format(deletedEdge, addedEdge1, addedEdge2, minDamage, cost))
+
+
 
 
 
