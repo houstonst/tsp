0ba7e0ebd17bb6db16ef2666e224071d3630d627
houstonst <matthew.houston@westpoint.edu>
2019-09-05 23:22:14 -0400

    computes the farthest initial two-node tour

--- farthestInsertion.py
+++ farthestInsertion.py
@@ -1,8 +1,20 @@
 import operator, time, math
 from euclidean import *
 
+def initTour(weightedGraph): # Finds initial tour and cost between the two furthest away points
+  maxVal = 0                 # Works.
+  r = 0
+  c = 0
+  for row in weightedGraph:
+    if max(row) > maxVal:
+      maxVal = max(row)
+      r = weightedGraph.index(row)
+      c = row.index(maxVal)
+  return (maxVal, (r, c))
+
 def farthestNeighbor(graph, nameArray):
   startTime = time.time()
   cost = 0.0
   wg = weightedGraph(graph)
   iterations = 0
+  return 0
