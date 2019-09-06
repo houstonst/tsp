c9755268f6c46743fc4b1cfc84e5b7676ac45778
houstonst <matthew.houston@westpoint.edu>
2019-09-05 23:48:10 -0400

    determines the minimum edge from a non-subtour node to the subtour nodes

--- farthestInsertion.py
+++ farthestInsertion.py
@@ -10,11 +10,27 @@ def initTour(weightedGraph): # Finds initial tour and cost between the two furth
       maxVal = max(row)
       r = weightedGraph.index(row)
       c = row.index(maxVal)
-  return (maxVal, (r, c))
+  return (maxVal, [r, c])
 
 def farthestNeighbor(graph, nameArray):
   startTime = time.time()
   cost = 0.0
   wg = weightedGraph(graph)
   iterations = 0
-  return 0
+  cost, path = initTour(wg)
+  path.append(path[0])
+
+  #see which node not in the subtour has the greatest minimum distance to a node in the subtour
+  legList = []
+  for i in range(len(graph)): #iterate through nodes
+    minLeg = 999999
+    if i not in path: #consider node n if not in subtour
+      for c in range(len(wg[i])): #look at distances from node n to any other node
+        if c in path and wg[i][c] < minLeg: #consider only edges that go from n to a subtour node that are less than the current minLeg
+          minLeg = wg[i][c]
+      legList.append((minLeg, i))
+  return legList
+
+
+
+  #return (cost, path)
