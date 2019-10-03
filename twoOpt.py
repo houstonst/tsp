c56aeef807843bacdc3a58ba432f2ef3359409a3
houstonst <matthew.houston@westpoint.edu>
2019-10-03 00:11:51 -0400

    Highlights edges. Needs better conditionals

--- twoOpt.py
+++ twoOpt.py
@@ -15,6 +15,7 @@ def step(wg, path, cost, unchanged, i, j, lineList, wndw, graph):
       n = pathCopy[j+1]
       oCost = wg[a][b] + wg[m][n]
       pCost = wg[a][m] + wg[b][n]
+
       if pCost < oCost:
         if (a, b) in lineList.keys():
           wndw.delete(lineList[(a, b)])
@@ -35,6 +36,7 @@ def step(wg, path, cost, unchanged, i, j, lineList, wndw, graph):
 
         pathCopySlice = pathCopy[i+1:j]
         pathCopy = pathCopy[0:i+1] + [pathCopy[j]] + pathCopySlice[::-1] + pathCopy[j+1:]
+
         costCopy = costCopy - oCost + pCost
         unchanged = False
   return pathCopy, costCopy, unchanged, lineList
@@ -71,20 +73,91 @@ def twoOpt(graph, nameArray, path, cost):
     lineList.update({(node, nxt): a})
   a = w.create_line(graph[path[0]][0], graph[path[0]][1], last[0], last[1], fill = "black")
   lineList.update({(path[0], path[len(path)-1])})
+
+  w.itemconfig(lineList[(path[i], path[i+1])], fill = "red")
+  w.itemconfig(lineList[(path[j], path[j+1])], fill = "red")
   
   def stepper():
     nonlocal i, j, pathCopy, costCopy, unchanged, graph, lineList
+
     if j < len(graph):
+      a = pathCopy[i]
+      b = pathCopy[i+1]
+      c = pathCopy[j]
+      d = pathCopy[j+1]
+      if (a, b) in lineList.keys():
+        w.itemconfig(lineList[(a, b)], fill = "black")
+      elif (b, a) in lineList.keys():
+        w.itemconfig(lineList[(b, a)], fill = "black")
+      if (c, d) in lineList.keys():
+        w.itemconfig(lineList[(c, d)], fill = "black")
+      elif (d, c) in lineList.keys():
+        w.itemconfig(lineList[(d, c)], fill = "black")
+
       pathCopy, costCopy, unchanged, lineList = step(wg, pathCopy, costCopy, unchanged, i, j, lineList, w, graph)
+
       j += 1
     elif j == len(graph) and i < len(graph)-3:
       i += 1
       j = i + 2
+      
+      a = pathCopy[i]
+      b = pathCopy[i+1]
+      c = pathCopy[j]
+      d = pathCopy[j+1]
+      if (a, b) in lineList.keys():
+        w.itemconfig(lineList[(a, b)], fill = "black")
+      elif (b, a) in lineList.keys():
+        w.itemconfig(lineList[(b, a)], fill = "black")
+      if (c, d) in lineList.keys():
+        w.itemconfig(lineList[(c, d)], fill = "black")
+      elif (d, c) in lineList.keys():
+        w.itemconfig(lineList[(d, c)], fill = "black")
+
       pathCopy, costCopy, unchanged, lineList = step(wg, pathCopy, costCopy, unchanged, i, j, lineList, w, graph)
     if i == len(graph):
       i = 0
       j = 2
+
+      a = pathCopy[i]
+      b = pathCopy[i+1]
+      c = pathCopy[j]
+      d = pathCopy[j+1]
+      if (a, b) in lineList.keys():
+        w.itemconfig(lineList[(a, b)], fill = "black")
+      elif (b, a) in lineList.keys():
+        w.itemconfig(lineList[(b, a)], fill = "black")
+      if (c, d) in lineList.keys():
+        w.itemconfig(lineList[(c, d)], fill = "black")
+      elif (d, c) in lineList.keys():
+        w.itemconfig(lineList[(d, c)], fill = "black")
+
       pathCopy, costCopy, unchanged, lineList = step(wg, pathCopy, costCopy, unchanged, i, j, lineList, w, graph)
+    
+    u = i
+    v = j
+
+    if v == len(graph) and u < len(graph)-3:
+      u += 1
+      v = u + 2
+    if u == len(graph):
+      u = 0
+      v = 2
+
+    if v < len(graph)-3: #better handle this case!!!! Causes instances without highlighting
+      a = pathCopy[u]
+      b = pathCopy[u+1]
+      c = pathCopy[v]
+      d = pathCopy[v+1]
+
+      if (a, b) in lineList.keys():
+        w.itemconfig(lineList[(a, b)], fill = "red")
+      elif (b, a) in lineList.keys():
+        w.itemconfig(lineList[(b, a)], fill = "red")
+      if (c, d) in lineList.keys():
+        w.itemconfig(lineList[(c, d)], fill = "red")
+      elif (d, c) in lineList.keys():
+        w.itemconfig(lineList[(d, c)], fill = "red")
 
   # TKINTER #
   stepButton = Button(root, text = "Step", command = stepper)
