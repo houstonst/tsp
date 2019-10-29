96b41c2308e620ca9a4f78f4e0136b3ae1ac1b82
houstonst <matthew.houston@westpoint.edu>
2019-10-29 05:36:34 -0400

    Readjusts coordinates to fit GUI

--- algos/reader.py
+++ algos/reader.py
@@ -12,4 +12,31 @@ def reader(file):
       coords.append([int(float(rowArray[1])), int(float(rowArray[2]))]) #python cannot convert from string representation
       cityNames.append(rowArray[0])                                     #of a float to an int. Convert to float first.
   
+  coords = fitter(coords, 750, 1200)
   return cityNames, coords
+
+
+def fitter(coords, height, width): #force coordinates to fit GUI window size
+  maxWidth = 0
+  maxHeight = 0
+  widthMult = 1
+  heightMult = 1
+
+  for coord in coords:
+    if coord[0] > maxWidth:
+      maxWidth = coord[0]
+    if coord[1] > maxHeight:
+      maxHeight = coord[1]
+
+  if maxWidth >= width - 100:
+    widthMult = (width-100)/maxWidth
+  if maxHeight >= height - 100:
+    heightMult = (height-100)/maxHeight
+
+  for coord in coords:
+    coord[0] = coord[0]*widthMult
+    coord[1] = coord[1]*heightMult
+  
+  return coords
+
+  
